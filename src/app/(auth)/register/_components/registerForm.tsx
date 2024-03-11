722ca07669d5e237de78ac9afd4632292476e41f
lutfiandri <lutfiandri232@gmail.com>
2024-03-11 18:03:15 +0700

    feat(auth): create register screen

---
+++ src/app/(auth)/register/_components/registerForm.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { FormSchema, formSchema, submitForm } from '../_actions/register';
+
+export default function RegisterForm() {
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(submitForm)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="you@example.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Full Name</FormLabel>
+              <FormControl>
+                <Input placeholder="your name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="your password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirm_password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="confirm your password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Register</Button>
+      </form>
+    </Form>
+  );
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,21 @@
+import { Separator } from '@/components/ui/separator';
+import LoginForm from './_components/registerForm';
+import Link from 'next/link';
+
+export default function RegisterPage() {
+  return (
+    <div className="grid place-items-center min-h-screen">
+      <div className="flex flex-col w-full md:w-[600px] gap-8">
+        <h1 className="text-3xl">Login to Lutfi&apos;s SmartHome</h1>
+        <LoginForm />
+        <Separator className="-mb-4" />
+        <div className="text-sm">
+          Already have an account?{' '}
+          <Link href="/login" className="text-blue-500 hover:text-blue-700">
+            Login &rarr;
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
